b162c079d5900bdcf34739bd94a41d4d073abc2e
Teodor Danielsen <teodor.danielsen@gmail.com>
2020-02-18 19:07:02 +0100

    setting minHeight based on window.innerHeight

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { Logo } from './Logo'
 
+function useWindowHeight() {
+  const [height, setHeight] = useState(0)
+  useLayoutEffect(() => {
+    function updateHeight() {
+      setHeight(window.innerHeight)
+    }
+    window.addEventListener('resize', updateHeight)
+    updateHeight()
+    return () => window.removeEventListener('resize', updateHeight)
+  }, [])
+  return height
+}
+
 export function App() {
+  const height = useWindowHeight()
   return (
     <div>
-      <div className={'banner'}>
+      <div className={'banner'} style={{ minHeight: height }}>
         <Logo />
         <p>by Teodor Nilseng Danielsen</p>
       </div>
